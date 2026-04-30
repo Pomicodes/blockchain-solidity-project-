0625b33a9de2592cb7a1a9df7f8863840a7b4c66
Gadisa Kasaye <gadisaka04@gmail.com>
2026-04-30 11:05:00 +0300

    Escrow events + funding hooks

---
+++ Code/B.Address_Interactions/4.Escrow/security.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.20;
+
+contract Escrow {
+    address public depositor;
+    address public beneficiary;
+    address public arbiter;
+
+    constructor(address _arbiter, address _beneficiary) payable {
+        depositor = msg.sender;
+        arbiter = _arbiter;
+        beneficiary = _beneficiary;
+    }
+
+    function approve() external {
+        require(msg.sender == arbiter);
+        (bool success, ) = beneficiary.call{value: address(this).balance}("");
+        require(success);
+    }
+}
