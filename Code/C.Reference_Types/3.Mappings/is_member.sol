9d9c612a8fd66a5e932aa858d5f19df3b676a711
Sosina Seifu <sosina.seifu@astu.edu.et>
2026-05-03 10:25:00 +0300

    Mapping add/remove flows

---
+++ Code/C.Reference_Types/3.Mappings/is_member.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.20;
+
+contract Contract {
+    mapping(address => bool) public members;
+
+    function addMember(address user) external {
+        members[user] = true;
+    }
+
+    function isMember(address user) external view returns (bool) {
+        return members[user];
+    }
+}
